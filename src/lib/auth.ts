a5d37f411df0e21a60e2b9c80756dfcb3fb493a0
CharitSinghChauhan <charitsinghchauhan@gmail.com>
2026-02-19 15:12:21 +0530

    fix/env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,6 +22,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: `${backendUrl}/api/auth/callback/google`,
     },
   },
 });
